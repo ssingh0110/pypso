b25cc3aaa726882d4d95f5b1e8f7225456e1f890
Shashank <shashanksingh0110@gmail.com>
2018-12-30 17:03:58 +0530

    Updated PSO

--- pypso.py
+++ pypso.py
@@ -1,21 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # Variables Initialization
-VarMin = -10 	# Lower Bound
-VarMax = +10 	# Upper Bound
-Dim = 5 		# dimension for PSO 
-GlobalBestCost = 1000000000000000000000000000000 # Random Large Number
+VarMin = -10.0 	# Lower Bound
+VarMax = 10.0 	# Upper Bound
+MaxVel = 0.2*(VarMax - VarMin)
+MinVel = -MaxVel
+Dim = 10 		# dimension for PSO 
+GlobalBestCost = 10000 # Random Large Number
 RecordedBestCost = []
+
+# Clerc and Kennedy (2002) Constriction Coefficient
+k = 1
+phi_1 = 2.05
+phi_2 = 2.05
+phi = phi_1 + phi_2
+chi = (2*k)/(np.abs(2-phi-np.sqrt(phi**2 - 4*phi)))
+
 # PSO parameters
 global nPop
 global Maxit
 Maxit = 1000	# Maximum iteration
 nPop = 50	    # Population size
-w = 1 			# Inertia coefficient
-damp = 0.99		# Damping ration of inertia
-c1 = 2			# Personal acceleration coefficient
-c2 = 2 			# Social acceleration coefficient
+w = chi 			# Inertia coefficient
+damp = 1		# Damping ration of inertia
+c1 = chi*phi_1			# Personal acceleration coefficient
+c2 = chi*phi_2 			# Social acceleration coefficient
 
 # Objective Function
 def Cost(arr):
@@ -67,7 +78,8 @@ for i in range(0, nPop):
 # Plotting Results
 def plot(arr):
 	plt.semilogy(arr)
-	plt.grid(b=bool, which="both", axis="both", color = 'g', linestyle = '--')
+	plt.grid(b=bool, which="both", \
+		axis="both", color = 'g', linestyle = '--')
 	plt.xlabel("Iteration")
 	plt.ylabel("Best Cost")
 	plt.title("PSO Implementation in Python")
@@ -87,12 +99,25 @@ for i in range(0, Maxit):
 		c_2 = GlobalBestPosition - particle[j].position
 
 		particle[j].velocity = w*a \
-					+ c1*np.multiply(b_1, b_2) \
-					+ c2*np.multiply(c_1, c_2) 
+			+ c1*np.multiply(b_1, b_2) \
+			+ c2*np.multiply(c_1, c_2) 
+		
+		# Apply Limits to Velocity
+		particle[j].velocity =  np.maximum(\
+			particle[j].velocity, MinVel) 
+		particle[j].velocity =  np.minimum(\
+			particle[j].velocity, MaxVel)
 
 		# Update Position
 		particle[j].position = particle[j].position \
-					+ particle[j].velocity
+			+ particle[j].velocity
+
+ 		# Apply Limits to Position
+		particle[j].position =  np.maximum(\
+			particle[j].position, VarMin)
+
+		particle[j].position =  np.minimum(\
+			particle[j].position, VarMax)
 
 		# Evaluation of Cost
 		particle[j].cost = Cost(particle[j].position)
@@ -107,7 +132,7 @@ for i in range(0, Maxit):
 			GlobalBestCost = particle[j].best.cost
 			GlobalBestPosition = particle[j].best.position
 
-		# Best Cost
+	# Best Cost
 	print "Iter: %d"%i, " Best Cost: ", GlobalBestCost
 	#print "Best Position: ",GlobalBestPosition
 	RecordedBestCost.append(GlobalBestCost)
